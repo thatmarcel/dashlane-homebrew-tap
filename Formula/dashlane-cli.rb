13d0e2b48ce230db1a6e4a8d32c5e86914b08841
Spiridon Robakidze <spiridon.robakidze@dashlane.com>
2026-04-14 17:25:05 +0200

    Update dashlane-cli URL and sha256 to v6.2614.0 (#17)
    Signed-off-by: Spiridon Robakidze <spiridon.robakidze@dashlane.com>

--- Formula/dashlane-cli.rb
+++ Formula/dashlane-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DashlaneCli < Formula
   desc "Command-line interface for Dashlane"
   homepage "https://dashlane.com"
-  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v6.2612.0.tar.gz"
-  sha256 "d10b96627d9c29b532576a270f68ff673981f5b585b86cbf0c2cde71140620af"
+  url "https://github.com/Dashlane/dashlane-cli/archive/refs/tags/v6.2614.0.tar.gz"
+  sha256 "943ba342fe2da6ce62fc977d4e83622d0259911d43ec679c62f91c8b7e5a9941"
   license "Apache-2.0"
 
   livecheck do
