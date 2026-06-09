b2f5cbf6a73ff88875c83f84e7983efaf5e93823
Marcel Braun <contact@thatmarcel.com>
2026-06-09 12:33:16 +0200

    Update dashlane-cli.rb
    Signed-off-by: Marcel Braun <contact@thatmarcel.com>

--- Formula/dashlane-cli.rb
+++ Formula/dashlane-cli.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class DashlaneCli < Formula
   desc "Command-line interface for Dashlane"
   homepage "https://dashlane.com"
@@ -12,34 +10,14 @@ class DashlaneCli < Formula
     strategy :github_latest
   end
 
-  option "with-corepack", "Use yarn from corepack instead of installing it"
-
-  depends_on "node@22" => :build
-
-  # Needed for node-gyp packages
-  depends_on "python" => :build
-  depends_on "python-setuptools" => :build
-
-  depends_on "yarn" if !build.with?("corepack")
-  depends_on "corepack" if  build.with?("corepack")
-
   on_macos do
     # macos 12+ only
     depends_on macos: :monterey
   end
 
   def install
-    ENV["COMMIT_HASH"] = "c377153edeacd7a328204fd73cad8a825f228cc3"
-    Language::Node.setup_npm_environment
-    platform = OS.linux? ? "linux" : "macos"
-    system "yarn", "set", "version", "4.5.1"
-    system "yarn", "install", "--frozen-lockfile"
-    system "yarn", "run", "build"
-    system "yarn", "workspaces", "focus", "--production"
-    system "yarn", "dlx", "@yao-pkg/pkg@6.1.1", "./dist",
-      "-t", "node22-#{platform}-#{Hardware::CPU.arch}", "-o", "bin/dcli",
-      "--no-bytecode", "--public", "--public-packages", "tslib,thirty-two,node-hkdf-sync,vows"
-    bin.install "bin/dcli"
+    system "wget", "https://github.com/Dashlane/dashlane-cli/releases/download/v6.2614.0/dcli-macos-arm64", "dcli"
+    bin.install "dcli"
   end
 
   test do
